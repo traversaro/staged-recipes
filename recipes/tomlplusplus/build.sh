71f63d12fe08a45e0fff966a3875ed7386928cb2
Silvio Traversaro <silvio@traversaro.it>
2021-05-15 09:24:27 +0200

    Update build.sh

--- recipes/tomlplusplus/build.sh
+++ recipes/tomlplusplus/build.sh
@@ -1,4 +1,4 @@
-meson builddir ${MESON_ARGS} --prefix=$PREFIX
+meson builddir ${MESON_ARGS} -Dgenerate_cmake_config=true -Dbuild_tests=false -Dbuild_examples=false --prefix=$PREFIX
 cd builddir
 ninja
 ninja install
