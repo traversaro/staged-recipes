f896860fe20d1e3a93ea70c0e8237f1413e343ae
Silvio Traversaro <silvio@traversaro.it>
2021-05-15 09:23:03 +0200

    Create bld.bat

---
+++ recipes/tomlplusplus/bld.bat
@@ -0,0 +1,22 @@
+setlocal EnableDelayedExpansion
+@echo on
+
+:: meson options
+set ^"MESON_OPTIONS=^
+  --prefix="%LIBRARY_PREFIX%" ^
+  --backend=ninja ^
+ ^"
+
+:: configure build using meson
+%BUILD_PREFIX%\python.exe %BUILD_PREFIX%\Scripts\meson setup builddir !MESON_OPTIONS!
+if errorlevel 1 exit 1
+
+:: print results of build configuration
+%BUILD_PREFIX%\python.exe %BUILD_PREFIX%\Scripts\meson configure builddir
+if errorlevel 1 exit 1
+
+ninja -v -C builddir -j %CPU_COUNT%
+if errorlevel 1 exit 1
+
+ninja -C builddir install -j %CPU_COUNT%
+if errorlevel 1 exit 1
